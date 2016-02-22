cc2c7917e60ecabe9314b5f40e2f425291661319
mwoodson2 <mikeonewoodson@gmail.com>
2016-02-22 05:39:15 -0800

    Final

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+
+$('.roundedImage').click(function() {
+  $('section').hide();
+  $(this).css('opacity', '0.5');
+  $('div').not(this).css('opacity', '1' );
+  $(this).next().slideToggle().css('display', 'inline-block');
+});
+
+$('.cloud').click(function(event) {
+ // Get url from the <a> href attribute
+    window.open("rank25chat.html","_self"); // Open the url in the current window. Set to "_blank" instead of "_self" to open in a new window.
+    // event.preventDefault(); // Prevent default action (e.g. following the link)
+    // return false;
+});
+
+
+
+}); //end ready
